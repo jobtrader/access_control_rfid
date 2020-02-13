ae2bd759bc7e98f3c57013dfe725f1b1720c6f8c
jobtrader <job.masker@gmail.com>
2020-02-13 16:25:41 +0700

    add out gate permission

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pytz
 import datetime
 from fastapi import FastAPI
-from model import get_employee_info, insert_inout_transaction
+from model import get_employee_info_in, insert_inout_transaction, get_employee_info_out
+import requests
+from config import config
+import json
+
 
 app = FastAPI()
 
@@ -11,10 +15,34 @@ def read_root():
 
 @app.post("/access/rfid/{gate_number}")
 def read_item(gate_number: str, id: str = None, type: str = None):
-    permission_info = get_employee_info(id)
     dt = datetime.datetime.now(pytz.timezone('Asia/Bangkok'))
     print("Request at:", datetime.datetime.now(pytz.timezone('Asia/Bangkok')))
+    gate_type = config('config.ini', 'DoorType')
+    gate_in = eval(gate_type['in'])
+    gate_out = eval(gate_type['out'])
+    if gate_number in gate_in:
+        permission_info = get_employee_info_in(id)
+    elif gate_number in gate_out:
+        permission_info = get_employee_info_out(id)
+    else:
+        print('Wrong gate name')
+        return 0
     if permission_info:
         insert_inout_transaction(permission_info, type, "Machine_Area", dt, "Z1-3")
+        gate_ip = config('config.ini', 'Gate')
+        raw_gate_number = gate_number
+        gate_number = gate_number.lower()
+        if gate_number in gate_ip:
+            gate_ip = config('config.ini', 'Gate')[gate_number]
+            url = 'http://{0}/display?type={1}&id={2}&gate={3}'.format(gate_ip, type, id, raw_gate_number)
+            response = requests.get(url)
+            print('Request to display @ {0}'.format(gate_ip))
+            result = json.loads(response.text)
+            print(result)
+            if result['result'] == 'complete':
+                print('Displayed Credential Info of ID = {0} at Gate = {1}'.format(id, gate_number))
+
+            else:
+                print('Failed to Display Credential Info of ID = {0} at Gate = {1}'.format(id, gate_number))
         return 1
     return 0
--- model.py
+++ model.py
@@ -2,11 +2,21 @@ from config import config
 import pyodbc
 
 
+def get_employee_info_out(rfid):
+    connection_info = config('config.ini', 'Database')
+    driver = connection_info['driver']
+    server_name = connection_info['server_name']
+    database = connection_info['database_name']
+    uid = connection_info['uid']
+    password = connection_info['password']
+    statement = "SELECT RFID.ID_RFID, Contractor.IDCard, Contractor.contractno, Contractor.FirstName, Contractor.LastName FROM dbo.RFID AS RFID INNER JOIN dbo.Contractor AS Contractor ON RFID.IDCard = Contractor.IDCard  WHERE (RFID.ID_RFID LIKE '%{0}%')".format(rfid)
+    conn = pyodbc.connect('DRIVER={0};SERVER={1};DATABASE={2};UID={3};PWD={4}'.format(driver, server_name, database, uid, password))
+    cursor = conn.cursor()
+    cursor.execute(statement)
+    return cursor.fetchone()
 
 
-
-
-def get_employee_info(rfid):
+def get_employee_info_in(rfid):
     connection_info = config('config.ini', 'Database')
     driver = connection_info['driver']
     server_name = connection_info['server_name']
@@ -14,8 +24,7 @@ def get_employee_info(rfid):
     uid = connection_info['uid']
     password = connection_info['password']
     statement = "SELECT RFID.ID_RFID, Contractor.IDCard, Contractor.contractno, Contractor.FirstName, Contractor.LastName, Permission.Sub_Zone_Name, Permission.Date_exp FROM dbo.RFID AS RFID INNER JOIN dbo.Contractor AS Contractor ON RFID.IDCard = Contractor.IDCard INNER JOIN dbo.Permission AS Permission ON RFID.ID_RFID = Permission.ID_RFID WHERE (RFID.ID_RFID LIKE '%{0}%') AND  Permission.Sub_Zone_Name = 'Machine_Area'  AND Permission.Date_exp between  GETDATE() and  Permission.Date_exp".format(rfid)
-    conn = pyodbc.connect('DRIVER={0};SERVER={1};DATABASE={2};UID={3};PWD={4}'.format(driver, server_name, database,
-                                                                uid, password))
+    conn = pyodbc.connect('DRIVER={0};SERVER={1};DATABASE={2};UID={3};PWD={4}'.format(driver, server_name, database, uid, password))
     cursor = conn.cursor()
     cursor.execute(statement)
     return cursor.fetchone()
@@ -39,5 +48,6 @@ def insert_inout_transaction(emp_info, type, door, dt, place_work):
 if __name__ == '__main__':
     # pass
     # print(get_permission_info(5167))
-    print(get_employee_info('142F'))
+    # print(get_employee_info_in('142F'))
+    print(get_employee_info_out('2c862'))
     # insert_inout_transaction(('6700379F93', '3471200157410', 'Pieceworkตามการจ้างงาน', 'สมพร', 'ทองแกม', 'shutdown_TL', datetime.datetime(2019, 12, 25, 23, 59, 59)), "IN", "Machine_Area",datetime.datetime.now(pytz.timezone('Asia/Bangkok')), "Z1-3")
